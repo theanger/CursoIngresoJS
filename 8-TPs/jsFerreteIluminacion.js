67150faee3d69500cfd3add8523f42a8768d1809
theanger <31898638+theanger@users.noreply.github.com>
2019-07-29 17:41:44 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -9,6 +9,49 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 
  */
 function CalcularPrecio () 
-{
- 	
+{ var Lamparas= parseInt(35);
+  var Cantidad= parseInt(document.getElementById("Cantidad").value);
+  var Marca= document.getElementById("Marca").value;
+  var Valor= parseInt (Lamparas* Cantidad);
+  var descuento= parseInt;
+  var impuesto= parseInt;
+  
+ 
+  if (Cantidad >=6)
+  { descuento = Valor * 50 /100    
+   document.getElementById("precioDescuento").value= Valor- descuento; }
+ 
+  else if (Cantidad == 5 && Marca == "ArgentinaLuz" )
+   { descuento= Valor * 40/100  
+     document.getElementById("precioDescuento").value= Valor- descuento;  }
+
+    else if (Cantidad==5 && Marca != "ArgentinaLuz")
+    { descuento= Valor* 30 / 100     
+     document.getElementById("precioDescuento").value= Valor- descuento; }
+
+      else if (Cantidad==4  && Marca == "ArgentinaLuz" || Marca == "FelipeLamparas" )  
+     { descuento= Valor * 25 /100     
+       document.getElementById("precioDescuento").value= Valor- descuento;  }
+     
+       else if  ( Cantidad==4 && Marca == "Osram" || Marca == "HazIluminacion" || Marca =="JeLuz" ) 
+       { descuento = Valor *20 /100 
+         document.getElementById("precioDescuento").value= Valor- descuento;  }
+
+         else if ( Cantidad==3 && Marca== "ArgentinaLuz") 
+          { descuento= Valor* 15/100
+            document.getElementById("precioDescuento").value= Valor- descuento;  }
+            
+             if (Cantidad==3 && Marca== "FelipeLamparas")
+            { descuento= Valor* 10 /100
+             document.getElementById("precioDescuento").value= Valor- descuento; }
+
+             else if (Cantidad== 3 && Marca == "Osram" || Marca == "HazIluminacion" || Marca == "JeLuz" )
+             { descuento= Valor* 5 /100
+                document.getElementById("precioDescuento").value= Valor- descuento;   }
+                if ( Valor- descuento > 120 )
+                { impuesto= (Valor- descuento) * 10 /100
+                  document.getElementById("precioDescuento").value= (Valor- descuento)+ impuesto;
+                  alert (" Usted pago "  +   ( Valor-descuento )  + " siendo %10 el impuesto, total de " + impuesto ) ;
+                    }
+
 }
