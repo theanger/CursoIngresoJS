9e6b46a2514c05777eba4dc9d28f68bd6f469cb6
theanger <31898638+theanger@users.noreply.github.com>
2019-08-05 22:08:29 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,17 +1,41 @@
 function mostrar()
 {
 
-	var contador=0;
+	//var contador=0;
 	// declarar variables
 	
-	var respuesta='si';
+	//var respuesta='si';
 
-	while(respuesta!='no')
-	{
-		
+	//while(respuesta!='no')
 	
-	}
+	var num;
+	var maximo= parseInt(0);
+	var minimo= parseInt(0);
+	var respuesta= "si"
+	
+	do {        
+		num= parseInt (prompt("ingrese un numero"));
+		respuesta= prompt ("Desea seguir ingresando numeros?"); 
+		
+	  //while ( isNaN (num) ) {
+		//num= parseInt(prompt("Error de validacion, ingrese un numero") );
+	 
+	 	
+		if ( flag == 0 ) {  
+		  minimo= num;
+		  maximo=num;
+		  flag = 1; }
+		
+		  if ( numero > maximo) { 
+			 maximo= numero+ maximo;  }
 
+			 if (numero < minimo)  {   
+				 minimo= numero+ minimo;  }
+				 
+			 respuesta= prompt ("Desea seguir ingresando numeros?");
+			 } while (respuesta=="si");
+			 
+		  
 
 
 
